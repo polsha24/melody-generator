fcc2acde00d38f950279907f6b7b37c36aadca1e
Alexandra Badasyan <sashabadasyan@icloud.com>
2025-12-22 15:15:15 +0300

    Fixed comments about imports again

--- src/services/visualizer.py
+++ src/services/visualizer.py
@@ -8,7 +8,7 @@ from typing import Optional, Union
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
-from ..entities.melody import Melody  # from ..entities import Melody
+from ..entities.melody import Melody  # from entities import Melody
 
 NOTE_NAMES_SHARP = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 NOTE_NAMES_FLAT = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
